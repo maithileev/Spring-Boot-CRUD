8fc0dc8a4b260fbf0b507a9f9d6b9bcd5350f4a8
Hp <Hp@192.168.1.5>
2020-06-27 23:31:56 +0530

    Basic CRUD done

--- Courses/src/main/java/com/example/demo/controller/CourseController.java
+++ Courses/src/main/java/com/example/demo/controller/CourseController.java
@@ -1,8 +1,14 @@
 package com.example.demo.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,6 +33,36 @@ public class CourseController {
 		}
 		else {
 			System.out.println("Not added");
+		}
+	}
+	
+	@GetMapping(value = "/{id}")
+	public CourseModel getCourse(@PathVariable Long id) {
+		CourseModel course = service.getCourse(id);
+		return course; 
+	}
+	
+	@GetMapping(value = "/OurCourses")
+	public List getCourses() {
+		List<CourseModel> courses= service.getCourses();
+		return courses;
+	}
+	
+	@PutMapping(value = "/UpdateCourse/{id}")
+	public CourseModel updateCourse(@RequestBody CourseModel c , @PathVariable Long id) {
+		CourseModel courseModel = service.updateCourse(id, c);
+		return courseModel;
+	}
+	
+	@DeleteMapping(value = "/RemoveCourse/{id}")
+	public void deleteCourse(@PathVariable Long id) {
+		boolean result = service.deleteCourse(id);
+		
+		if(result == true) {
+			System.out.println("Deleted");
+		}
+		else {
+			System.out.println("Something went wrong");
 		}
 	}
 }
--- Courses/src/main/java/com/example/demo/service/CourseService.java
+++ Courses/src/main/java/com/example/demo/service/CourseService.java
@@ -1,5 +1,9 @@
 package com.example.demo.service;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,4 +20,36 @@ public class CourseService {
 		CourseModel course = courseRepository.save(courseModel);
 		return course != null?true:false;
 	}
+	
+	public CourseModel getCourse(Long id) {
+		CourseModel course = courseRepository.findById(id).get();	
+		return course;
+	}
+	
+	public List getCourses() {
+		List<CourseModel> courses = new ArrayList<>();
+		courses = courseRepository.findAll();
+		return courses;
+	}
+	
+	public CourseModel updateCourse(Long id, CourseModel c) {
+		CourseModel course = courseRepository.findById(id).get();
+		course.setCourseDuration(c.getCourseDuration());
+		course.setCourseName(c.getCourseName());
+		course.setCourseStatus(c.getCourseStatus());
+		courseRepository.save(course);
+		return course; 
+	}
+	
+	public boolean deleteCourse(Long id) {
+		Optional<CourseModel> course = courseRepository.findById(id);
+		
+		if(course.isPresent()) {
+		courseRepository.delete(course.get());
+		return true;
+		}
+		else
+			return false;
+	}
+	
 }
